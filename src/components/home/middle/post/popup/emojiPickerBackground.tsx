113a16f18926396cd4b65ae546a010c62b4b4e0f
Armanur Rahman <mubeenarman19@gmail.com>
2023-05-07 22:20:29 +0900

    implement background in post

--- src/components/home/middle/post/popup/emojiPickerBackground.tsx
+++ src/components/home/middle/post/popup/emojiPickerBackground.tsx
@@ -1,6 +1,17 @@
 import { useEffect, useRef, useState } from "react";
 import Picker, { EmojiClickData } from "emoji-picker-react";
 
+const postBackgroundImages = [
+    "../../../images/postbackgrounds/1.jpg",
+    "../../../images/postbackgrounds/2.jpg",
+    "../../../images/postbackgrounds/3.jpg",
+    "../../../images/postbackgrounds/4.jpg",
+    "../../../images/postbackgrounds/5.jpg",
+    "../../../images/postbackgrounds/6.jpg",
+    "../../../images/postbackgrounds/7.jpg",
+    "../../../images/postbackgrounds/8.jpg",
+    "../../../images/postbackgrounds/9.jpg",
+];
 interface Props {
     firstName: string;
     status: string;
@@ -14,9 +25,10 @@ const EmojiPickerBackground: React.FC<Props> = ({
     isAddingImage,
 }) => {
     const [showEmoji, setShowEmoji] = useState(false);
-
+    const [showBGs, setShowBGs] = useState(false);
     const statusRef = useRef<HTMLTextAreaElement>(null);
     const [currentCursor, setCurrentCursor] = useState(0);
+    const bgRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
         if (statusRef.current && statusRef.current.selectionEnd) {
@@ -35,9 +47,15 @@ const EmojiPickerBackground: React.FC<Props> = ({
         setCurrentCursor(start.length + emoji.length);
     };
 
+    const bgHandler = (i: number) => {
+        if (bgRef.current?.style) {
+            bgRef.current.style.backgroundImage = `url(${postBackgroundImages[i]})`;
+            bgRef.current.classList.add("bg_handler");
+        }
+    };
     return (
         <div
-            className={`${
+            className={` ${
                 isAddingImage ? "post_popup_input_area_image_only" : ""
             }`}
         >
@@ -45,20 +63,51 @@ const EmojiPickerBackground: React.FC<Props> = ({
                 className={`post_popup_text_area ${
                     isAddingImage ? "post_popup_text_area_image_only" : ""
                 }`}
+                ref={bgRef}
             >
                 <textarea
                     placeholder={`What is on your mind ${firstName}?`}
                     value={status}
                     onChange={(e) => setStatus(e.target.value)}
                     ref={statusRef}
+                    maxLength={250}
+                    style={{
+                        paddingTop:
+                            showBGs && statusRef.current?.value
+                                ? Math.abs(
+                                      statusRef.current?.value.length * 0.1 - 30
+                                  )
+                                : "",
+                    }}
                 ></textarea>
             </div>
 
             <div className='post_popup_input_action'>
                 {!isAddingImage && (
-                    <img src='../../../icons/colorful.png' alt='' />
+                    <img
+                        src='../../../icons/colorful.png'
+                        alt=''
+                        onClick={() => setShowBGs(true)}
+                    />
                 )}
-
+                {!isAddingImage && showBGs && (
+                    <div
+                        className='post_popup_input_action_none'
+                        onClick={() => setShowBGs(false)}
+                    ></div>
+                )}
+                {!isAddingImage && showBGs && (
+                    <>
+                        {postBackgroundImages.map((bg, i) => (
+                            <img
+                                src={bg}
+                                key={i}
+                                alt=''
+                                onClick={() => bgHandler(i)}
+                            />
+                        ))}
+                    </>
+                )}
                 <i
                     className='emoji_icon_large'
                     onClick={() => setShowEmoji((prevState) => !prevState)}
